893a65782e0164738c04f7719537239f175a104f
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:47:12 +0530

    Update and rename question 4.py to question 04.py

--- question 04.py
+++ question 04.py
@@ -4,7 +4,11 @@ Created on Fri Sep  1 15:55:45 2023
 
 @author: LAB
 """
-
+'''
+4. Take a positive integer N as input followed by repeatedly taking numbers from the user till the
+time user entered -999. At the end display the count of input numbers that are divisible by N
+and the count of input numbers that are not divisible by N.
+'''
 n = int(input("enter a number N : "))
 s = 0
 t = 0
@@ -17,7 +21,7 @@ while t != -999:
     else:
         v = v+1
     s = t+s
-print("sum of all numers till now is",s+999) 
+#print("sum of all numers till now is",s+999) 
 print("numbers divided by N are :",c)
 print("numbers not divided by N are :",v)
     
